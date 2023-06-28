bb243ea27a98ceb35b5b9870babd671b17b64f37
shanon <shanonmambo@gmail.com>
2023-06-28 14:00:25 -0400

    deploy aws

--- next.config.js
+++ next.config.js
@@ -5,8 +5,35 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: [
+    "medusa-public-images.s3.eu-west-1.amazonaws.com", 
+    "localhost",
+    "medusa-server-ninjas.s3.amazonaws.com",
+    "https://cms.01ninjas.com",
+    "https://demo.01ninjas.com"
+
+  ],
   },
 })
+
+module.exports = {
+  projectConfig: {
+    // ...other configurations
+    store_cors: process.env.STORE_CORS.split(","),
+  },
+};
+
+
+
+
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
