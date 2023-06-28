eca0f37fe6734f3ab8aa68da02fa6f9e3bb9005e
shanon <shanonmambo@gmail.com>
2023-06-28 15:07:48 -0400

    ignore

--- next.config.js
+++ next.config.js
@@ -26,14 +26,14 @@ module.exports = withStoreConfig({
 
 
 
-// module.exports = {
-//   typescript: {
-//     // !! WARN !!
-//     // Dangerously allow production builds to successfully complete even if
-//     // your project has type errors.
-//     // !! WARN !!
-//     ignoreBuildErrors: true,
-//   },
-// };
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
